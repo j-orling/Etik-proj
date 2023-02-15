f337647963d9a41ece3649e5f74b234f31bcff32
josie <83011032+j-orling@users.noreply.github.com>
2023-02-15 13:42:41 +0100

    Add files via upload

---
+++ Josefin_Örling_Assignment1_Code/Josefin_Örling_Assignment1_Code/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josefin_Örling_Assignment1_Code
+{
+    class Node
+    {
+        public List<Item> allItems;
+        public List<Item> itemsPacked = new List<Item>();
+        public Node parent;
+        public List<Node> children = new List<Node>();
+        public int totalValue;
+        public int totalWeight;
+
+        public Node(Node parent, List<Item> allItems, List<Item> itemsPacked)
+        {
+            this.allItems = allItems;
+            if(itemsPacked != null)
+            {
+                this.itemsPacked = itemsPacked;
+            }
+            
+            for(int i = 0; i < this.itemsPacked.Count; i++)
+            {
+                totalValue += this.itemsPacked[i].value;
+                totalWeight += this.itemsPacked[i].weight;
+            }
+            if(parent != null)
+            {
+                this.parent = parent;
+            }
+
+        }
+
+    }
+}
---
+++ Josefin_Örling_Assignment1_Code/Josefin_Örling_Assignment1_Code/Tree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josefin_Örling_Assignment1_Code
+{
+    class Tree
+    {
+        public List<Item> allItems;
+        public List<Node> allNodes;
+        public int maxWeight = 420;
+        public Tree(List<Item> items)
+        {
+            allItems = items;
+            allNodes = new List<Node>();
+        }
+
+        public Node BFS()
+        {
+            List<Item> rootItem = new List<Item>();
+            rootItem.Add(allItems[0]);
+            Node root = new Node(null, allItems, rootItem);
+            Queue<Node> tree = new Queue<Node>();
+            tree.Enqueue(root);
+            Node bestPath = root;
+            while(tree.Any())
+            {
+                Node n = tree.Dequeue();
+                for(int i = 0; i < n.itemsPacked.Count; i++)
+                {
+                    Console.WriteLine(n.itemsPacked[i].id);
+                }
+                if(n.totalWeight <= maxWeight && n.totalValue > bestPath.totalValue)
+                {
+                    bestPath = n;
+                    Console.WriteLine("The new best path has weight " + bestPath.totalWeight + " and value " + bestPath.totalValue);
+                }
+
+                for (int i = 0; i < allItems.Count; i++)
+                {
+                    if (!n.itemsPacked.Contains(allItems[i]))
+                    {
+                        
+                    }
+                }
+
+
+                for (int i = 0; i < n.children.Count; i++)
+                {
+                    tree.Enqueue(n.children[i]);
+                }
+            }
+
+            return bestPath;
+        }
+    }
+}
